b113b94ded89b8c85fdda687ed61ff425c03c049
Maximilian <scientificresearch309@gmail.com>
2023-09-07 22:49:54 +0200

    my solution to the exercise works well!

--- index.js
+++ index.js
@@ -16,3 +16,26 @@ console.log("After...");
 // Callbacks
 // Promises
 // Async/await
+
+// const getRepositories = (username) => {
+//   return ["repo1", "repo2", "repo3"];
+// };
+
+// 1. convert this function to an Asynchronous function:
+
+const repo = ["repo1", "repo", "repo3"];
+const getRepositories = (username, callback) => {
+  setTimeout(() => {
+    console.log("----------------------------------------");
+    console.log("Reading repositories from Github...");
+    callback({
+      username: username,
+      repo: ["repo1", "repo", "repo3"],
+    });
+  }, 2000);
+};
+console.log("Before...");
+getRepositories("Max", (repo) => {
+  console.log("Repo", repo);
+});
+console.log("After...");
