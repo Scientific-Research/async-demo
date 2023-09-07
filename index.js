e8db089cc8f0c957689cd8321bcaff7562c7a59e
Maximilian <scientificresearch309@gmail.com>
2023-09-08 00:21:07 +0200

    just replace callback with promises and how to consume it, comes later!

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-const getUser = (id, callback) => {
-  return new Promise((resolve,reject)=>)
-  setTimeout(() => {
-    console.log("Reading a user from a database...");
-    callback({
-      id: id,
-      gitHubUsername: "Maxi",
-    });
-  }, 2000);
+const getUser = (id) => {
+  return new Promise((resolve, reject) => {
+    // Kick off some async work
+    setTimeout(() => {
+      console.log("Reading a user from a database...");
+      resolve({
+        id: id,
+        gitHubUsername: "Maxi",
+      });
+    }, 2000);
+  });
 };
-
-// Callbacks
-// Promises
-// Async/await
-
-// const getRepositories = (username) => {
-//   return ["repo1", "repo2", "repo3"];
-// };
-
-// 1. convert this function to an Asynchronous function:
-
+/////////////////////////////////////////////////////////
 const repo = ["repo1", "repo2", "repo3"];
-const getRepositories = (username, callback) => {
-  setTimeout(() => {
-    console.log("---------------------------------------------------");
-    console.log("Reading repositories from Github...");
-    callback({
-      username: username,
-      // repo: ["repo1", "repo2", "repo3"],
-      repositories: repo,
-    });
-  }, 2000);
+const getRepositories = (username) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("---------------------------------------------------");
+      console.log("Reading repositories from Github...");
+      resolve({
+        username: username,
+        // repo: ["repo1", "repo2", "repo3"],
+        repositories: repo,
+      });
+    }, 2000);
+  });
 };
-
+/////////////////////////////////////////////////////////
+const getCommits = (repo) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling GitHub API...");
+      resolve(["commit"]);
+    }, 2000);
+  });
+};
 /////////////////////////////////////////////////////////
 console.log("Before...");
 getUser(1, (user) => {
