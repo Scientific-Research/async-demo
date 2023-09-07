53bf870ec1fd8577d0b968bd5763a39de9e61fa3
Maximilian <scientificresearch309@gmail.com>
2023-09-07 16:38:29 +0200

    Patterns for dealing with Asynchronous Code => Callbacks, Promises, Async/await

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const getUser = (id) => {
+  setTimeout(() => {
+    console.log("Reading a user from a database...", id);
+    return {
+      id: id,
+      gitHubUsername: "Maxi",
+    };
+  }, 2000);
+  return 1;
+};
 console.log("Before...");
-setTimeout(() => {
-  console.log("Reading a user from a database...");
-}, 2000);
+const user = getUser(1);
+console.log(user);
 console.log("After...");
+
+// Callbacks
+// Promises
+// Async/await
