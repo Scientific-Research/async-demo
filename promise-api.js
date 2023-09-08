e04b749e6619909a7d1e751a80dbbfad9a8284b7
Maximilian <scientificresearch309@gmail.com>
2023-09-08 12:28:06 +0200

    using race to show which Promise is already finished !

--- promise-api.js
+++ promise-api.js
@@ -4,11 +4,11 @@
 // const r = Promise.reject(("reason for rejection..."));
 // r.catch((error) => console.log(error));
 
-const p1 = new Promise((resolve, reject) => {
+const p1 = new Promise((resolve) => {
   setTimeout(() => {
     console.log("Async operation 1 ...");
-    // resolve(16);
-    reject(new Error("because something failed...!"));
+    resolve(16);
+    // reject(new Error("because something failed...!"));
   }, 2000);
 });
 
@@ -19,6 +19,6 @@ const p2 = new Promise((resolve) => {
   }, 2000);
 });
 
-Promise.all([p1, p2])
+Promise.race([p1, p2])
   .then((result) => console.log(result))
   .catch((err) => console.log("Error", err.message));
