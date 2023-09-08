f6c461014ea474f7b118bf3a316eaa1fae85c888
Maximilian <scientificresearch309@gmail.com>
2023-09-08 11:51:18 +0200

    Promise.all() - is a parallel Promise processing...

--- promise-api.js
+++ promise-api.js
@@ -1,5 +1,21 @@
-const p = Promise.resolve({ id: 1 });
-p.then((result) => console.log(result));
+// const p = Promise.resolve({ id: 1 });
+// p.then((result) => console.log(result));
 
-const r = Promise.reject(("reason for rejection..."));
-r.catch((error) => console.log(error));
+// const r = Promise.reject(("reason for rejection..."));
+// r.catch((error) => console.log(error));
+
+const p1 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 1 ...");
+    resolve(16);
+  }, 2000);
+});
+
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 2 ...");
+    resolve(25);
+  }, 2000);
+});
+
+Promise.all([p1, p2]).then((result) => console.log(result));
